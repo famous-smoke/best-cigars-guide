413f028ede451c9888145fc8f2001d3fb7135be0
Tony Klapatch <aklapatch@famous-smoke.com>
2024-05-08 15:23:31 -0400

    LSD-1502 Add footer block styling, logo, colors

--- best-cigars-guide/blocks/footer/footer.js
+++ best-cigars-guide/blocks/footer/footer.js
@@ -1,5 +1,26 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+import { isInternal } from '../../scripts/scripts.js';
+
+function getFamousLogo() {
+  // Create the image element
+  const image = document.createElement('img');
+  image.src = '/best-cigars-guide/icons/famous-smoke-shop-logo-gray.png';
+  image.className = 'footer-logo';
+
+  // Add the Famous link, with the logo inside of it
+  const link = document.createElement('a');
+  link.href = 'https://www.famous-smoke.com';
+  link.className = 'footer-logo-link';
+  link.appendChild(image);
+
+  // Wrap it up
+  const wrap = document.createElement('div');
+  wrap.className = 'footer-logo-wrap';
+  wrap.appendChild(link);
+
+  return wrap;
+}
 
 /**
  * loads and decorates the footer
@@ -15,6 +36,21 @@ export default async function decorate(block) {
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  // Add the Famous logo
+  footer.prepend(getFamousLogo());
+
+  // Open external links in new tab
+  footer.querySelectorAll('a').forEach((a) => {
+    const href = a.getAttribute('href');
+    if (!isInternal(href)) {
+      a.setAttribute('target', '_blank');
+    }
+  });
+
+  // Set copyright year to current year
+  const currentYear = new Date().getFullYear();
+  footer.innerHTML = footer.innerHTML.replaceAll('{year}', currentYear.toString());
 
   block.append(footer);
 }
