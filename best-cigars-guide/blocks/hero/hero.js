8e5be552df6d192a62297989cb5da1cf11ab0fef
Matthew Gold <mgold@famous-smoke.com>
2024-06-04 09:10:49 -0400

    Add second paragraph and clickable chevron to hero. Style hero text.

---
+++ best-cigars-guide/blocks/hero/hero.js
@@ -0,0 +1,68 @@
+/* eslint-disable max-len */
+
+// Add second paragraph text and chevron to the hero image when available
+function addSecondParagraphToHero() {
+  // Select the div with class 'hero-container'
+  const heroContainer = document.querySelector('.hero-container');
+  const currentDomain = window.location.origin;
+
+  // Find the first non-empty paragraph after the hero-container
+  let firstNonEmptyParagraph = null;
+  let nextElement = heroContainer.nextElementSibling;
+  while (nextElement) {
+    if (nextElement.tagName === 'P' && nextElement.textContent.trim() !== '') {
+      firstNonEmptyParagraph = nextElement;
+      break;
+    }
+    nextElement = nextElement.firstElementChild ? nextElement.firstElementChild : nextElement.nextElementSibling;
+  }
+
+  // Find the first h1 on the page
+  const firstH1 = document.querySelector('h1');
+
+  // Append the found paragraph to the first h1
+  if (firstH1 && firstNonEmptyParagraph) {
+    firstH1.insertAdjacentElement('afterend', firstNonEmptyParagraph);
+
+    // Create the div element to contain the chevron image
+    const imgContainer = document.createElement('div');
+    imgContainer.style.display = 'inline-block';
+    imgContainer.style.cursor = 'pointer'; // Make cursor a hand
+
+    // Create the chevron image element
+    const img = document.createElement('img');
+    img.src = `${currentDomain}/best-cigars-guide/icons/arrow-down.png`;
+    img.alt = 'arrow';
+    img.className = 'arrow-down';
+
+    // Append the chevron image to the div
+    imgContainer.appendChild(img);
+
+    // Add click event to scroll to the first element with the class 'article-list-container' when available
+    const targetElement = document.querySelector('.article-list-container');
+    if (targetElement) {
+      imgContainer.addEventListener('click', () => {
+        if (targetElement) {
+          // Get the position of the target element
+          const rect = targetElement.getBoundingClientRect();
+          // Get the header height from the CSS variable
+          const headerHeight = getComputedStyle(document.documentElement).getPropertyValue('--nav-height');
+          const headerOffset = headerHeight ? parseInt(headerHeight, 10) : 90; // Convert to integer or default to 90
+          const elementPosition = rect.top + window.scrollY;
+          const offsetPosition = elementPosition - headerOffset;
+
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+          });
+        }
+      });
+    }
+
+    firstNonEmptyParagraph.insertAdjacentElement('afterend', imgContainer);
+  }
+}
+
+export default function decorate() {
+  addSecondParagraphToHero();
+}
